9f2253d48ed4a949b063eb487599c9669de1f06d
Ian Burrell <ianburrell@gmail.com>
2011-11-15 20:13:39 -0800

    - Add options for Host, RiakOpts, Bucket - Separate update

--- lib/Apache/Session/Store/Riak.pm
+++ lib/Apache/Session/Store/Riak.pm
@@ -10,31 +10,40 @@ sub new
     my ($class, $session) = @_;
     my $self;
 
-    $self->{client} = Net::Riak->new(...);
-    $self->{bucket} = $self->{client}->bucket($opts->{bucket});
+    $self->{client} = Net::Riak->new(
+        host => $session->{args}->{Host},
+        %{ $session->{args}->{RiakOpts} },
+    );
+    $self->{bucket} = $self->{client}->bucket($session->{args}->{Bucket});
 
     bless $self, $class;
 }
 
 sub insert
 {
-    my ( $self, $session ) = @_;
-    my $object = $self->{bucket}->new_object($session->{data}{_sesssion_id}, $session->{serialized} );
+    my ($self, $session) = @_;
+    my $object = $self->{bucket}->new_object($session->{data}->{_sesssion_id}, $session->{serialized});
     $object->store();
 }
 
-*update = *insert;
+sub update
+{
+    my ($self, $session) = @_;
+    my $object = $self->{bucket}->get($session->{data}->{_session_id});
+    $object->value($session->{serialized});
+    $object->store();
+}
 
 sub materialize
 {
-    my ( $self, $session ) = @_;
+    my ($self, $session) = @_;
     my $object = $self->{bucket}->get($session->{data}->{_session_id});
     $session->{serialized} = $object->value();
 }
 
 sub remove
 {
-    my ( $self, $session ) = @_;
+    my ($self, $session) = @_;
     $self->{bucket}-delete_object($session->{data}->{_session_id});
 }
 
